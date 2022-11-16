4724ae4833cc2a95a51fcf111539f5bea28c5c6d
juano <juano@AcerAspire5Juan.home>
2022-11-16 21:45:43 +0100

    Añadida clase categoria, ampliada clase juego
    Se ha añadido la clase categoria y nuevos atributos para juego, entre
    ellos una relacion entre estas dos clases. También se ha iniciado la
    lógica de la organización de juegos por categoría en el catálogo

--- GGez_App/admin.py
+++ GGez_App/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from GGez_App.models import Juego
+from GGez_App.models import *
 # Register your models here.
 admin.site.register(Juego)
+admin.site.register(Categoria)
--- GGez_App/views.py
+++ GGez_App/views.py
@@ -3,4 +3,9 @@ from GGez_App.models import *
 # Create your views here.
 def catalogo(request):
     juegos = Juego.objects.all()
-    return render(request,'catalogo.html',{'catalogo':juegos})
+    categorias = []
+    for j in juegos:
+        cat = j.categoria
+        if cat not in categorias:
+            categorias.append(cat)
+    return render(request,'catalogo.html',{'catalogo':juegos,'categorias':categorias})
